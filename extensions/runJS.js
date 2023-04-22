caebe26416858f805b0f92ce50adea4007c6e16f
jackb0back <54455407+jackb0back@users.noreply.github.com>
2023-04-22 19:11:37 -0400

    Update runJS.js

--- extensions/runJS.js
+++ extensions/runJS.js
@@ -5,7 +5,7 @@
     getInfo () {
       return {
         id: 'SJS',
-        name: 'Scratch JS',
+        name: 'JavaScript',
         blocks: [
           {
             opcode: 'run',
@@ -16,8 +16,7 @@
                 type: Scratch.ArgumentType.STRING,
                 defaultValue: 'console.log("Hello, world!");'
               }
-            },
-            blockIconURI: 'data:image/svg+xml,%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 viewBox%3D%220 0 70 70%22%3E%3Crect width%3D%2270%22 height%3D%2270%22 fill%3D%22black%22%2F%3E%3C%2Fsvg%3E'
+            }
           }
         ]
       };
