fbd5c58b96857dfccad233d298fd5d1ba8329c81
jackb0back <54455407+jackb0back@users.noreply.github.com>
2023-04-22 19:02:35 -0400

    Create runJS.js

---
+++ extensions/runJS.js
@@ -0,0 +1,36 @@
+(function(Scratch) {
+  'use strict';
+
+  class JavaScriptCommand {
+    getInfo () {
+      return {
+        id: 'javascript_command',
+        name: 'JavaScript Command',
+        blocks: [
+          {
+            opcode: 'run',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Run JavaScript [code]',
+            arguments: {
+              code: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'console.log("Hello, world!");'
+              }
+            }
+          }
+        ]
+      };
+    }
+
+    run (args, util) {
+      const code = args.code;
+      try {
+        eval(code);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  Scratch.extensions.register(new JavaScriptCommand());
+})(Scratch);
